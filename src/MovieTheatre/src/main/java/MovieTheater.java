e5d5cb5149fe8d536aecffb368a669d4fe2a660f
waterfuried <cisternsystem@gmail.com>
2023-04-18 01:43:23 +0300

    Курс "Подготовка к собеседованию". Задание №4

--- src/MovieTheatre/src/main/java/MovieTheater.java
+++ src/MovieTheatre/src/main/java/MovieTheater.java
@@ -38,6 +38,30 @@ import com.sun.org.slf4j.internal.*;
             (сколько посетителей пришло с 9 до 15 часов и т.д.).
 */
 public class MovieTheater {
+    public static void main(String[] args) {
+        try {
+            // для корректной работы проекта
+            // - нужно подключить библиотеку SQLite
+            // - добавить для Maven зависимость в pom-документ/файл с настройками/конфигурацию
+            Class.forName("org.sqlite.JDBC");
+            connection = DriverManager.getConnection("jdbc:sqlite:MovieTheater.db");
+            Statement st = connection.createStatement();
+
+            getTimeTable();
+            getBreaks();
+            printTaskName("Задача №3 - общая и сеансовая статистика посещаемости - не реализована", false);
+            printTaskName("Посещения и сборы", false);
+            int[] times = new int[] { 9, 15, 18, 21, 24};
+            for (int i = 0; i < times.length - 1; i++)
+                getStats(times[i], times[i+1]);
+
+            st.close();
+            connection.close();
+        }
+        catch (ClassNotFoundException ex) { logger.error(ex.getMessage()); }
+        catch (SQLException ex) { logger.error("База данных кинотеатра не найдена!"); }
+    }
+
     final static int TASK1 = 0, TASK2 = 1, TASK3 = 2, TASK4 = 3;
 
     final static String[][] HEADER_COLUMN = new String[][] {
@@ -47,15 +71,15 @@ public class MovieTheater {
             { "Время начала", "Зрители", "Сборы" }
     };
 
-    static int[] MAX_LENGTH = new int[HEADER_COLUMN.length];
+    static int[] MAX_LENGTH = new int[HEADER_COLUMN[TASK1].length];
 
     // поскольку табуляция имеет "плавающую" длину,
     // иногда она может состоять из 1 пробельного символа,
     // что не всегда удобно при чтении
-    static String SEPARATOR = "   ";
+    final static String SEPARATOR = "   ";
 
     // общая часть SQL-запросов в задачах 1 и 2
-    static String SQL_ACCESS_TIMETABLE =
+    final static String SQL_ACCESS_TIMETABLE =
             "FROM films " +
             "LEFT JOIN sessions " +
             "ON sessions.film_id = films.id "+
@@ -65,16 +89,7 @@ public class MovieTheater {
 
     static Connection connection;
 
-    static String leadZero(int value) { return (value < 10 ? "0" : "") + value; }
-
-    static void printTaskName(String name, boolean first) {
-        if (!first) System.out.println();
-        // как ни странно, но метод repeat для строк был введен только с Java 11
-        System.out.println(name);
-        for (int i = 0; i < name.length(); i++) System.out.print('-');
-        System.out.println();
-    }
-
+    // задача 4
     static void getStats(int start, int end) {
         try (PreparedStatement ps = connection
                 .prepareStatement(
@@ -107,54 +122,7 @@ public class MovieTheater {
         } catch (SQLException ex) { logger.error(ex.getMessage()); }
     }
 
-    static String adjustToLength(String value, int length, int substIdx) {
-        StringBuilder sb =
-                new StringBuilder(value.endsWith("_")
-                        ? value.substring(0, value.length()-1)+substIdx
-                        : value);
-        while (sb.length() < length) sb.append(" ");
-        return sb.toString();
-    }
-
-    static String addHeaderColumns(int task, int index) {
-        StringBuilder sb = new StringBuilder();
-        for (int i = 0; i < HEADER_COLUMN[task].length; i++)
-            sb.append(SEPARATOR).append(adjustToLength(HEADER_COLUMN[task][i], MAX_LENGTH[i], index));
-        return sb.toString();
-    }
-
-    static void displayFilmSession(ResultSet rs, boolean firstPart) throws SQLException {
-        if (firstPart) System.out.print(SEPARATOR);
-        for (int j = 0; j < rs.getMetaData().getColumnCount(); j++)
-            System.out.print(adjustToLength(rs.getString(j+1),MAX_LENGTH[j],0)+SEPARATOR);
-    }
-
-    static long getTime(String datetime) {
-        SimpleDateFormat fmt = new SimpleDateFormat("yyyy-MM-dd HH:mm");
-        try { return fmt.parse(datetime).getTime(); }
-        catch (ParseException ex) {
-            ex.printStackTrace();
-            return 0L;
-        }
-    }
-
-    static void displayBreaks(ResultSet rs, String firstTime, int duration) throws SQLException {
-        String secondTime = rs.getString("showtime");
-        System.out.print(
-                adjustToLength(secondTime,Math.max(MAX_LENGTH[1],HEADER_COLUMN[TASK2][0].length()),0)
-                + SEPARATOR + (((getTime(secondTime)-getTime(firstTime))/1000/60)-duration));
-    }
-
-    static int checkTimeTable() {
-        int cnt = 0;
-        try (PreparedStatement ps = connection
-                .prepareStatement("SELECT COUNT(*) " + SQL_ACCESS_TIMETABLE)) {
-            ResultSet rs = ps.executeQuery();
-            if (rs.next()) cnt = rs.getInt(1);
-        } catch (SQLException ex) { logger.error(ex.getMessage()); }
-        return cnt;
-    }
-
+    // задача 1
     static void getTimeTable() {
         int cnt = checkTimeTable();
         if (cnt == 0) return;
@@ -202,6 +170,7 @@ public class MovieTheater {
         } catch (SQLException ex) { logger.error(ex.getMessage()); }
     }
 
+    // задача 2
     static void getBreaks() {
         int cnt = checkTimeTable();
         if (cnt == 0) return;
@@ -232,27 +201,64 @@ public class MovieTheater {
 
     }
 
-    public static void main(String[] args) {
-        try {
-            // для корректной работы проекта
-            // - нужно подключить библиотеку SQLite
-            // - добавить для Maven зависимость в pom-документ/файл с настройками/конфигурацию
-            Class.forName("org.sqlite.JDBC");
-            connection = DriverManager.getConnection("jdbc:sqlite:MovieTheater.db");
-            Statement st = connection.createStatement();
+    /*
+     вспомогательные методы
+     */
+    static String leadZero(int value) { return (value < 10 ? "0" : "") + value; }
 
-            getTimeTable();
-            getBreaks();
-            printTaskName("Задача №3 - общая и сеансовая статистика посещаемости - не реализована", false);
-            printTaskName("Посещения и сборы", false);
-            int[] times = new int[] { 9, 15, 18, 21, 24};
-            for (int i = 0; i < times.length - 1; i++)
-                getStats(times[i], times[i+1]);
+    static void printTaskName(String name, boolean first) {
+        if (!first) System.out.println();
+        // как ни странно, но метод repeat для строк был введен только с Java 11
+        System.out.println(name);
+        for (int i = 0; i < name.length(); i++) System.out.print('-');
+        System.out.println();
+    }
 
-            st.close();
-            connection.close();
+    static String adjustToLength(String value, int length, int substIdx) {
+        StringBuilder sb =
+                new StringBuilder(value.endsWith("_")
+                        ? value.substring(0, value.length()-1)+substIdx
+                        : value);
+        while (sb.length() < length) sb.append(" ");
+        return sb.toString();
+    }
+
+    static String addHeaderColumns(int task, int index) {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < HEADER_COLUMN[task].length; i++)
+            sb.append(SEPARATOR).append(adjustToLength(HEADER_COLUMN[task][i], MAX_LENGTH[i], index));
+        return sb.toString();
+    }
+
+    static void displayFilmSession(ResultSet rs, boolean firstPart) throws SQLException {
+        if (firstPart) System.out.print(SEPARATOR);
+        for (int j = 0; j < rs.getMetaData().getColumnCount(); j++)
+            System.out.print(adjustToLength(rs.getString(j+1),MAX_LENGTH[j],0)+SEPARATOR);
+    }
+
+    static long getTime(String datetime) {
+        SimpleDateFormat fmt = new SimpleDateFormat("yyyy-MM-dd HH:mm");
+        try { return fmt.parse(datetime).getTime(); }
+        catch (ParseException ex) {
+            ex.printStackTrace();
+            return 0L;
         }
-        catch (ClassNotFoundException ex) { logger.error(ex.getMessage()); }
-        catch (SQLException ex) { logger.error("База данных кинотеатра не найдена!"); }
+    }
+
+    static void displayBreaks(ResultSet rs, String firstTime, int duration) throws SQLException {
+        String secondTime = rs.getString("showtime");
+        System.out.print(
+                adjustToLength(secondTime,Math.max(MAX_LENGTH[1],HEADER_COLUMN[TASK2][0].length()),0)
+                        + SEPARATOR + (((getTime(secondTime)-getTime(firstTime))/1000/60)-duration));
+    }
+
+    static int checkTimeTable() {
+        int cnt = 0;
+        try (PreparedStatement ps = connection
+                .prepareStatement("SELECT COUNT(*) " + SQL_ACCESS_TIMETABLE)) {
+            ResultSet rs = ps.executeQuery();
+            if (rs.next()) cnt = rs.getInt(1);
+        } catch (SQLException ex) { logger.error(ex.getMessage()); }
+        return cnt;
     }
 }
